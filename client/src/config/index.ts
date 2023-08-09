4bfa9b84daac5296cb50d3a05b1284afe7cc145c
Ryan Eggleston <40816745+ryaneggz@users.noreply.github.com>
2023-08-09 10:46:12 -0500

    Update index.ts

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,8 +1,12 @@
 import marked from './marked';
 
+const SERVER_URL = process.env.REACT_APP_NODE_ENV === 'production' 
+  ? 'https://oss.promptengineers.ai' 
+  : 'http://localhost:8000'
+
 const config = {
   api: {
-    SERVER_URL: 'http://localhost:8000',
+    SERVER_URL: SERVER_URL,
   },
   marked: marked,
 }
